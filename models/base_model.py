b7d7a82dd3e689baae251ec69f92766a661855ab
Lance Desk <lncedesk@gmail.com>
2023-12-08 13:32:17 -0800

    BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """
 Module containing the BaseModel class.
 
-defines the BaseModel class, which serves as the base class for other classes.
+Defines the BaseModel class, which serves as the base class for other classes.
 """
 
 import uuid
@@ -15,7 +15,7 @@ class BaseModel:
 
     Public instance attributes:
     - id (str): Assigned with a unique ID using uuid.uuid4().
-    - created_at (datetime): Aurrent datetime when an instance is created.
+    - created_at (datetime): Current datetime when an instance is created.
     - updated_at (datetime): Current datetime when an instance is created
       and updated every time the object changes.
 
@@ -38,13 +38,43 @@ class BaseModel:
     {'id': 'unique_id', 'created_at':
     'ISO_datetime', 'updated_at': 'ISO_datetime', '__class__': 'BaseModel'}
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes instance attributes.
+
+        Args:
+            *args: Variable length argument list (not used in this case).
+            **kwargs: Keyword args to initialize attributes from a dictionary.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the object.
+
+        Returns:
+            dict: A dictionary containing keys/values of the instance.
+        """
+        model_dict = self.__dict__.copy()
+        model_dict['__class__'] = self.__class__.__name__
+        model_dict['created_at'] = self.created_at.isoformat()
+        model_dict['updated_at'] = self.updated_at.isoformat()
+        return model_dict
 
     def __str__(self):
         """
@@ -62,16 +92,3 @@ class BaseModel:
         Updates the 'updated_at' attribute with the current datetime.
         """
         self.updated_at = datetime.now()
-
-    def to_dict(self):
-        """
-        Returns a dictionary representation of the object.
-
-        Returns:
-            dict: A dictionary containing keys/values of the instance.
-        """
-        model_dict = self.__dict__.copy()
-        model_dict['__class__'] = self.__class__.__name__
-        model_dict['created_at'] = self.created_at.isoformat()
-        model_dict['updated_at'] = self.updated_at.isoformat()
-        return model_dict
