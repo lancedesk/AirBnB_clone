d4cde75dedbdec6c59c32db40c7dd9e7027a5845
Lance Desk <lncedesk@gmail.com>
2023-12-09 08:16:45 -0800

    Edit user file

--- models/user.py
+++ models/user.py
@@ -7,7 +7,10 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class that inherits from BaseModel"""
+    """
+    User class that inherits from BaseModel
+    """
+
     email = ""
     password = ""
     first_name = ""
