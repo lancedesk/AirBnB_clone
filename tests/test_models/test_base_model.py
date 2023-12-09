f674cbc419f99e4119e88f1c5b9837affe14c35d
Lance Desk <lncedesk@gmail.com>
2023-12-09 07:36:37 -0800

    test_base_model update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -82,5 +82,147 @@ class TestBaseModel(unittest.TestCase):
         obj = BaseModel()
         self.assertLessEqual(obj.created_at, obj.updated_at)
 
+    # Additional Tests
+    def test_moduleDocs(self):
+        """
+        Tests documentation for the module.
+        """
+        moduleDoc = __import__("models.base_model").base_model.__doc__
+        self.assertGreater(len(moduleDoc), 0)
+
+    def test_classDocs(self):
+        """
+        Tests documentation for the class.
+        """
+        classDoc = __import__("models.base_model").base_model.BaseModel.__doc__
+        self.assertGreater(len(classDoc), 0)
+
+    def test_methodDocsSave(self):
+        """
+        Tests documentation for the save method.
+        """
+        methodDoc = (
+            __import__("models.base_model")
+            .base_model.BaseModel.save.__doc__
+        )
+        self.assertGreater(len(methodDoc), 0)
+
+    def test_methodDocsto_dict(self):
+        """
+        Tests documentation for the to_dict method.
+        """
+        methodDoc = (
+            __import__("models.base_model")
+            .base_model.BaseModel.to_dict.__doc__
+        )
+        self.assertGreater(len(methodDoc), 0)
+
+    def test_methodDocs__str___(self):
+        """
+        Tests documentation for the __str__ method.
+        """
+        methodDoc = (
+            __import__("models.base_model")
+            .base_model.BaseModel.__str__.__doc__
+        )
+        self.assertGreater(len(methodDoc), 0)
+
+    def test_idType(self):
+        """
+        Tests the type of the id attribute.
+        """
+        obj = BaseModel()
+        self.assertIs(type(obj.id), str)
+
+    def test_idLength(self):
+        """
+        Tests the length of the id attribute.
+        """
+        obj = BaseModel()
+        self.assertEqual(len(obj.id), 36)
+
+    def test_idValidity(self):
+        """
+        Tests the validity of the id attribute as a UUID.
+        """
+        obj = BaseModel()
+        value = UUID(obj.id)
+        self.assertIs(type(value), UUID)
+
+    def test_created_atType(self):
+        """
+        Tests the type of the created_at attribute.
+        """
+        obj = BaseModel()
+        self.assertIs(type(obj.created_at), datetime)
+
+    def test_updated_atType(self):
+        """
+        Tests the type of the updated_at attribute.
+        """
+        obj = BaseModel()
+        self.assertIs(type(obj.updated_at), datetime)
+
+    def test_outputOf__str__(self):
+        """
+        Tests the output of the __str__ method.
+        """
+        obj = BaseModel()
+        str1 = f"[BaseModel] ({obj.id}) {obj.__dict__}"
+        str2 = str(obj)
+        self.assertEqual(str1, str2)
+
+    def test_updated_atChanged(self):
+        """
+        Tests if calling save changes the updated_at attribute.
+        """
+        obj = BaseModel()
+        initial_value = obj.updated_at
+        obj.save()
+        self.assertGreater(obj.updated_at, initial_value)
+
+    def test_to_dictCheck(self):
+        """
+        Tests if the keys of to_dict are present in the object's __dict__.
+        """
+        obj = BaseModel()
+        to_dict_dict = obj.to_dict()
+        __dict__dict = obj.__dict__
+        for keys in __dict__dict:
+            self.assertIn(keys, to_dict_dict)
+
+    def test_to_dict(self):
+        """
+        Tests the presence and type of keys in the to_dict output.
+        """
+        obj = BaseModel()
+        to_dict_dict = obj.to_dict()
+        self.assertIn("__class__", to_dict_dict)
+        self.assertIs(type(to_dict_dict["__class__"]), str)
+
+    def test_to_dict_Valid(self):
+        """
+        Tests the validity and formatting of to_dict output.
+        """
+        obj = BaseModel()
+        to_dict_dict = obj.to_dict()
+        created_at = datetime.fromisoformat(to_dict_dict["created_at"])
+        updated_at = datetime.fromisoformat(to_dict_dict["updated_at"])
+        self.assertIn("created_at", to_dict_dict)
+        self.assertIn("updated_at", to_dict_dict)
+        self.assertIs(type(to_dict_dict["created_at"]), str)
+        self.assertIs(type(to_dict_dict["updated_at"]), str)
+        self.assertEqual(to_dict_dict["created_at"], created_at.isoformat())
+        self.assertEqual(to_dict_dict["updated_at"], updated_at.isoformat())
+
+    def test_dictType(self):
+        """
+        Tests the type of the id attribute in the to_dict output.
+        """
+        obj = BaseModel()
+        to_dict_dict = obj.to_dict()
+        self.assertIs(type(to_dict_dict["id"]), str)
+
+
 if __name__ == "__main__":
     unittest.main()
